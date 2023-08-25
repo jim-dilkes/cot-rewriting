4bbf5eda348650af01f0805189aa578fb84c61ab
Jim Booth-Dilkes <jsdilkes@googlemail.com>
2023-08-25 14:17:39 +0100

    New experiments in run and analysis

--- run_experiments.sh
+++ run_experiments.sh
@@ -6,30 +6,39 @@ taskChoices=(
     # "tracking_shuffled_objects/seven_objects"
     # "coinflip/eight"
     # "strategyqa"
-    # "prontoqa"
-    # "lsat-ar"
-    # "logiqa-en"
+    "prontoqa"
+    "lsat-ar"
+    "logiqa-en"
 )
 
-numExamples=5
-asyncConcurr=1
+numExamples=300
+asyncConcurr=7
 
 # Uncomment the below line to enable overwriting results
-overwriteResults="--overwrite_results"
-# overwriteResults=""
+# overwriteResults="--overwrite_results"
+overwriteResults=""
 
-runIdentifier="test_HF"
+ambiguousIncorrect="--ambiguous_incorrect"
+# ambiguousIncorrect=""
+
+runIdentifier="stg4"
 modelDefnsFiles=(
-    "PromptWithAnswerExtraction/llama7b_cot_instruct"
-    # "GoalExtraction/gpt35_goal_approach"
-    # "GoalExtraction/gpt35_goal_approach_sbs"
-    # "PromptWithAnswerExtraction/gpt35_cot_instruct"
-    # "GoalExtraction/gpt35_goal_answertype"
+"PromptWithAnswerExtraction/gpt35_cot_sbs"
+"PromptWithAnswerExtraction/gpt35_cot_instruct"
+"GoalExtraction/gpt35_goal_approach_sbs"
+"GoalExtraction/gpt35_goal_approach_instruct"
+"GoalExtraction/gpt35_approach_sbs"
+"GoalExtraction/gpt35_goal_sbs"
+"GoalExtraction/gpt35_goal_approach_sbs_gapattern"
+"GoalExtraction/gpt35_goal_approach_promptpattern"
+"GoalExtraction/gpt35_goal_approach_bothpattern"
+"SolveValidateRewrite/gpt35_all_instruct"
+"SolveValidateRewrite/gpt35_all_pattern"
 )
 
 for task in "${taskChoices[@]}"; do
     for modelDefns in "${modelDefnsFiles[@]}"; do
-        command="python run.py --task_name $task --model_defns_file $modelDefns  --run_identifier $runIdentifier  --num_examples $numExamples --async_concurr $asyncConcurr $overwriteResults"
+        command="python run.py --task_name $task --model_defns_file $modelDefns  --run_identifier $runIdentifier  --num_examples $numExamples --async_concurr $asyncConcurr $overwriteResults $ambiguousIncorrect"
         echo $command
         $command
     done
