57a5b7f8c432a9c1253eb3aea14b181bd1b3775f
Jim Booth-Dilkes <jsdilkes@googlemail.com>
2023-08-25 17:23:04 +0100

    Add multiple choice versions to bigbench tasks

--- src/task_utils.py
+++ src/task_utils.py
@@ -10,7 +10,10 @@ def get_task_answer_prompt(task_name, ambiguous_incorrect=False):
         if task_name == "gsm8k":
             return "Respond with the given single value that is the answer to the problem. Do not explain your answer or include symbols. If there is no answer or multiple answers respond with NA."
         elif task_name[: len("tracking_shuffled_objects")] == "tracking_shuffled_objects":
-            return "Respond with the correct given completion of the problem statement. Do not include names. Give only the entity that completes the final sentence. If the answer is ambiguous, respond with NA."
+            if task_name[-len("multi"):]== "multi":
+                return "Respond with exactly the given multiple choice answer to the question. Do not explain your answer only use A/B/C/D/E. If there is no answer or multiple answers respond with NA. A/B/C/D/E/NA:"
+            else:
+                return "Respond with the correct given completion of the problem statement. Do not include names. Give only the entity that completes the final sentence. If the answer is ambiguous, respond with NA."
         elif task_name[: len("coinflip")] == "coinflip":
             return "Respond with the given final state of the coin. Do not explain your answer only use heads or tails. If there is no answer or multiple answers respond with NA."
         elif task_name == "strategyqa":
@@ -46,7 +49,11 @@ def load_task(task_name, task_dir):
         task_name == "strategyqa"
         or task_name[: len("tracking_shuffled_objects")] == "tracking_shuffled_objects"
     ):
-        questions, answers = load_bigbench("task.json", filedir=task_dir)
+        if task_name[-len("_multi"):]== "_multi":
+            task_dir = task_dir[:-len("_multi")]
+            questions, answers = load_bigbench_multichoice("task.json", filedir=task_dir)
+        else:
+            questions, answers = load_bigbench("task.json", filedir=task_dir)
     elif task_name[: len("coinflip")] == "coinflip":
         questions, answers = load_coinflip("task.csv", filedir=task_dir)
     elif task_name == "prontoqa":
@@ -91,6 +98,24 @@ def load_bigbench(filename, filedir):
             if value == 1:
                 answers.append(answer)
                 break
+
+    print(f"{len(questions)} bigbench examples loaded from {file_path}")
+    return questions, answers
+
+def load_bigbench_multichoice(filename, filedir):
+    file_path = os.path.join(filedir, filename)
+    with open(file_path, "r") as f:
+        examples = json.load(f)["examples"]
+
+    questions = []
+    answers = []
+    for ex in examples:
+        options_str = '\n'.join(f"{chr(65 + i)}) {option}" for i, option in enumerate(ex['target_scores'].keys()))
+        question_string = f"{ex['input']}\n\n{options_str}"
+        questions.append(question_string)
+        correct_option_index = [i for i, score in enumerate(ex['target_scores'].values()) if score == 1][0]
+        correct_option_letter = chr(65 + correct_option_index)
+        answers.append(correct_option_letter)
 
     print(f"{len(questions)} bigbench examples loaded from {file_path}")
     return questions, answers
