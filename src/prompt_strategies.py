d41133d4b6cb693eeeb2009ce1ac7eb247457cf9
Jim Booth-Dilkes <jsdilkes@googlemail.com>
2023-09-02 00:42:33 +0100

    Add GoalApproachRewrite and SampleTree prompting strategies

--- src/prompt_strategies.py
+++ src/prompt_strategies.py
@@ -7,7 +7,7 @@ import sys
 
 def clean_answer(answer, task_name=None):
     if task_name == "gsm8k":
-        # Dont include "."
+        # Dont remove "."
         punctuation = string.punctuation.replace(".", "")
     else:
         punctuation = string.punctuation
@@ -18,9 +18,6 @@ class PromptStrategy(ABC):
     def __init__(self, model_messages_json, task_name):
         self.models = self.define_models(model_messages_json)
         self.task_name = task_name
-
-        # for k, v in kwargs.items():
-        #     setattr(self, k, v)
 
         try:
             self.required_model_tags  # Ensure that require_model_tags is defined
@@ -41,7 +38,7 @@ class PromptStrategy(ABC):
 
             model_name = model_inputs.pop("model_name")
             kwargs = model_inputs
-
+            
             models[model_tag] = md.get_model(model_name, **kwargs)
 
         return models
@@ -200,3 +197,147 @@ class GoalExtraction(PromptStrategy):
                 "answers": [answer],
                 "query_details": gen_respose_dicts,
             }
+
+
+
+class GoalApproachRewrite(PromptStrategy):
+    def __init__(self, model_messages_json, task_name, rewrite_trigger='no', max_rewrites=2):
+        self.max_rewrites = max_rewrites
+        self.rewrite_trigger = rewrite_trigger
+        self.required_model_tags = [
+            "goal_extractor",
+            "cot_generator",
+            "answer_extractor",
+            "validator",
+            "decider",
+            "rewriter"
+        ]
+        super().__init__(model_messages_json, task_name)
+
+    async def proc_example(
+        self, example: str, example_num: int, semaphore: asyncio.Semaphore
+    ):
+        async with semaphore:
+            answers_lst = []
+            cot_responses_lst = []
+            gen_respose_dicts = []
+            
+            # Goal and approach generation
+            goal_responses = await self.models["goal_extractor"].generate_async(example)
+            goal_approach_text = goal_responses['response']
+            gen_respose_dicts.append(goal_responses)
+
+            solution_content = f"Problem statement:{example}\n\nGoal and approach: {goal_approach_text}"
+
+            cot_response, answer, response_dict = await self.generate_answer(solution_content)
+            cot_responses_lst.append(cot_response)
+            answers_lst.append(answer)
+            gen_respose_dicts.extend(response_dict)
+
+            j = 0
+            while j < self.max_rewrites:
+                ## Validate the solution and decide whether to rewrite
+                cot_answer_content = "\n".join(
+                    [
+                        f"Proposed solution {i+1}: {r[0]}\nAnswer {i+1}: {r[1]}"
+                        for i, r in enumerate(zip(cot_responses_lst, answers_lst))
+                    ]
+                )
+
+                validation_content = (
+                    f"Problem statement: {example}\n\n{cot_answer_content}"
+                )
+                val_response_dict = await self.models["validator"].generate_async(
+                    validation_content
+                )
+                gen_respose_dicts.append(val_response_dict)
+
+                decision_content = (
+                    f"Solution validation: {val_response_dict['response']}"
+                )
+                decis_response_dict = await self.models["decider"].generate_async(
+                    decision_content
+                )
+                gen_respose_dicts.append(decis_response_dict)
+
+                if clean_answer(decis_response_dict["response"])[-3:] != self.rewrite_trigger:
+                    break
+
+                ## If we are rewriting, generate a new solution, using the previous solution as context
+                cot_content = f"{solution_content}\n\n: Validation of prior incorrect solution: {val_response_dict['response']}"
+                cot_response, answer, response_dict = await self.generate_answer(
+                    cot_content, cot_model="rewriter"
+                )
+                cot_responses_lst.append(cot_response)
+                answers_lst.append(answer)
+                gen_respose_dicts.extend(response_dict)
+
+                j += 1
+
+            print(f"\rDone example {example_num}", end="")
+            sys.stdout.flush()
+            
+            return {
+                "cot_responses": cot_responses_lst,
+                "answers": answers_lst,
+                "query_details": gen_respose_dicts,
+            }
+            
+
+
+class SampleTree(PromptStrategy):
+    def __init__(self, model_messages_json, task_name):
+        self.required_model_tags = [
+            "goal_extractor",
+            "approach_generator",
+            "step_executor",
+            "answer_extractor",
+        ]
+        super().__init__(model_messages_json, task_name)
+
+    async def proc_example(
+        self, example: str, example_num: int, semaphore: asyncio.Semaphore
+    ):
+        async with semaphore:
+            gen_respose_dicts = []
+            
+            text_problem_statement = f"### Problem Statement ###\n{example}"
+            
+            # Goal and approach generation
+            goal_response = await self.models["goal_extractor"].generate_async(text_problem_statement)
+            gen_respose_dicts.append(goal_response)
+            text_goal = f"### Goal ###\n{goal_response['response']}"
+
+            approach_response = await self.models["approach_generator"].generate_async(text_problem_statement+"\n"+text_goal)
+            gen_respose_dicts.append(approach_response)
+            
+            # Extract each row of the approach into a list
+            lst_approach = approach_response['response'].split("\n")
+
+            text_previous_steps = ""
+            for text_step in lst_approach:
+                
+                input = f"{text_problem_statement}\n### Previous Steps ###\n{text_previous_steps}### Step to Execute ###\n{text_step}"
+                
+                step_response = await self.models["step_executor"].generate_async(input)
+                gen_respose_dicts.append(step_response)
+                
+                format_step = step_response["response"].replace('\\n', ' ')
+                text_previous_steps += f"{format_step}\n"
+            
+        
+        text_cot = f"{text_problem_statement}\n{text_goal}### Steps ###\n{text_previous_steps}"
+        answer_response = await self.models["answer_extractor"].generate_async(text_cot)
+        gen_respose_dicts.append(answer_response)
+
+        print(f"\rDone example {example_num}", end="")
+        sys.stdout.flush()
+            
+        return {
+            "cot_responses": [text_cot],
+            "answers": [clean_answer(answer_response["response"], self.task_name)],
+            "query_details": gen_respose_dicts,
+        }
+            
+            
+            
